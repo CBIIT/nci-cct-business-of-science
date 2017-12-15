ed327d4835b121f69d265a84d914faf68dd8ddd1
srikiran <skphp9@gmail.com>
2017-12-15 12:53:55 -0500

    Setting file permissions

--- set_permissions.sh
+++ set_permissions.sh
@@ -1,7 +1,7 @@
 cd ..
-chmod 755 nci-metastatic-niche
-for d in nci-metastatic-niche; do find $d -type d -exec chmod 755 {} \;; find $d -type f -exec chmod 644 {} \;; done
-cd nci-metastatic-niche/
+chmod 755 nci-cct-business-of-science
+for d in nci-cct-business-of-science; do find $d -type d -exec chmod 755 {} \;; find $d -type f -exec chmod 644 {} \;; done
+cd nci-cct-business-of-science/
 chmod 644 settings.php
 chmod -R 775 files
 for d in files; do find $d -type d -exec chmod 775 {} \;; find $d -type f -exec chmod 664 {} \;; done
